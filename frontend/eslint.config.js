e469e0712942ed898e3eb5c0623aadbec5718b9f
Rasmus Puls <14924038+rpuls@users.noreply.github.com>
2026-02-22 03:28:00 +0100

    dependency maintenance

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,6 +4,10 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
+const browserGlobals = Object.fromEntries(
+  Object.entries(globals.browser).map(([name, value]) => [name.trim(), value]),
+)
+
 export default tseslint.config(
   { ignores: ['dist'] },
   {
@@ -11,7 +15,7 @@ export default tseslint.config(
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: browserGlobals,
     },
     plugins: {
       'react-hooks': reactHooks,
